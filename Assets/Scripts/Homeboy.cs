2bd415ac1cf86f683e35d06f5e783c54a7d5e5f5
Dominio95 <dominiktoedling95@gmail.com>
2017-03-13 17:56:54 +0100

    Added Unity project properly

---
+++ Assets/Scripts/Homeboy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homeboy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawCube(Vector3.zero, Vector3.one);
+    }
+}
